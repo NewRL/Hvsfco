aea2ae174e53655df09b2561fbdc2f9d8ff27dac
Jlopez <jlopez@hvsfco.com>
2019-09-10 17:28:55 -0600

    Common-Update

---
+++ theme_hospital/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Hospital Theme',
+    'description': 'Hospital Theme',
+    'category': 'Theme/Services',
+    'summary': 'Corporate, Business, Tech, Services',
+    'sequence': 333,
+    'version': '1',
+    'depends': ['theme_common', 'website_animate'],
+    'data': [
+        'views/assets.xml',
+        'views/customize_modal.xml',
+        
+    ],
+    'images': [
+        'static/description/hospital_description.jpg',
+        'static/description/hospital_screenshot.jpg',
+    ],
+    
+}
