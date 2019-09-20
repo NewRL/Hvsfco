5b38413167788e0b68fa770c80d0528dfdfd3ec2
Jlopez <jlopez@hvsfco.com>
2019-09-20 15:34:42 -0600

    HSP-TEST

---
+++ Hospital Managment System/acs_hms_hospitalization/models/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+
+from . import hospital_base
+from . import hms_base
+from . import surgery
+from . import hospitalization
+from . import hospitalization_care
+from . import death_register
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
